168543e966b36b75c7145fccfc4d57c6658b682b
Ammonite96 <slipknot_rul@mail.ru>
2020-03-02 23:10:43 +0500

    lec8 Exception extension

---
+++ TestProject/src/ru/JavaRush/lvl9/lec8/CatchingExceptions.java
@@ -0,0 +1,63 @@
+package ru.JavaRush.lvl9.lec8;
+
+import java.io.CharConversionException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileSystemException;
+
+/**
+ * 1. Разберись, какие исключения бросает метод BEAN.methodThrowExceptions.
+ * 2. Метод handleExceptions должен вызывать метод BEAN.methodThrowExceptions и обрабатывать исключения:
+ * 2.1. если возникло исключение FileSystemException, то логировать его (вызвать метод BEAN.log) и пробросить дальше
+ * 2.2. если возникло исключение CharConversionException или любое другое IOException, то только логировать его (вызвать метод BEAN.log)
+ * 3. Добавь в объявление метода handleExceptions класс исключения, которое ты пробрасываешь в п.2.1.
+ * 4. В методе main обработай оставшееся исключение - логируй его. Используй try..catch
+ *
+ * Подсказка:
+ * Если ты захватил исключение MyException, которое не хотел захватывать, его можно пробросить дальше кодом вида:
+ * catch (MyException e) {
+ *  throw e;
+ * }
+ */
+
+public class CatchingExceptions {
+    public static StatelessBean BEAN = new StatelessBean();
+
+    public static void main(String[] args) {
+        try {
+            handleExceptions();
+        } catch (FileSystemException f){
+            BEAN.log(f);
+        }
+    }
+
+    public static void handleExceptions() throws FileSystemException {
+        try {
+            BEAN.methodThrowExceptions();
+        } catch (FileSystemException fl){
+            BEAN.log(fl);
+            throw fl;
+        } catch (CharConversionException c){
+            BEAN.log(c);
+        } catch (IOException io){
+            BEAN.log(io);
+        }
+    }
+
+    public static class StatelessBean {
+        public void log(Exception exception) {
+            System.out.println(exception.getMessage() + ", " + exception.getClass().getSimpleName());
+        }
+
+        public void methodThrowExceptions() throws CharConversionException, FileSystemException, IOException {
+            int i = (int) (Math.random() * 3);
+            if (i == 0) {
+                throw new CharConversionException();
+            } else if (i == 1) {
+                throw new FileSystemException("");
+            } else if (i == 2) {
+                throw new IOException();
+            }
+        }
+    }
+}
---
+++ TestProject/src/ru/JavaRush/lvl9/lec8/CatchingUncheckedException.java
@@ -0,0 +1,48 @@
+package ru.JavaRush.lvl9.lec8;
+
+import ru.JavaRush.LvL5.Solution;
+
+/**
+ * В методе handleExceptions обработайте все unchecked исключения.
+ * Нужно вывести стек-трейс возникшего исключения используя метод printStack.
+ * Можно использовать только один блок try..catch
+ */
+
+public class CatchingUncheckedException {
+    public static void main(String[] args) {
+        handleExceptions(new CatchingUncheckedException());
+    }
+
+    public static void handleExceptions(CatchingUncheckedException obj) {
+        try {
+            obj.method1();
+            obj.method2();
+            obj.method3();
+        } catch (IndexOutOfBoundsException in){
+            printStack(in);
+        } catch (NumberFormatException num){
+            printStack(num);
+        } catch (NullPointerException nul){
+            printStack(nul);
+        }
+    }
+
+    public static void printStack(Throwable throwable) {
+        System.out.println(throwable);
+        for (StackTraceElement element : throwable.getStackTrace()) {
+            System.out.println(element);
+        }
+    }
+
+    public void method1() {
+        throw new NullPointerException();
+    }
+
+    public void method2() {
+        throw new IndexOutOfBoundsException();
+    }
+
+    public void method3() {
+        throw new NumberFormatException();
+    }
+}
