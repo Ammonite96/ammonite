db4be973f93f35852e7c049bd611ef48417447b5
Тимур <Ammonite96@users.noreply.github.com>
2020-03-26 23:02:12 +0500

    13.11

--- TestProject/src/ru/JavaRush/JavaCore/LvL13/lec11/BattlesRobots/AbstractRobot.java
+++ TestProject/src/ru/JavaRush/JavaCore/LvL13/lec11/BattlesRobots/AbstractRobot.java
@@ -1,4 +1,33 @@
 package ru.JavaRush.JavaCore.LvL13.lec11.BattlesRobots;
 
 public abstract class AbstractRobot implements Defensable, Attackable {
+
+    @Override
+    public BodyPart attack() {
+        BodyPart leg = BodyPart.LEG;
+        BodyPart arm = BodyPart.ARM;
+        if (AbstractRobot.this.name.)
+        return ;
+    }
+
+    @Override
+    public BodyPart defense() {
+        BodyPart head = BodyPart.HEAD;
+        BodyPart chest = BodyPart.CHEST;
+        return null;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public AbstractRobot(String name) {
+        this.name = name;
+    }
 }
--- TestProject/src/ru/JavaRush/JavaCore/LvL13/lec11/BattlesRobots/Robot.java
+++ TestProject/src/ru/JavaRush/JavaCore/LvL13/lec11/BattlesRobots/Robot.java
@@ -1,4 +1,8 @@
 package ru.JavaRush.JavaCore.LvL13.lec11.BattlesRobots;
 
 public class Robot extends AbstractRobot {
+
+    public Robot(String name) {
+        super(name);
+    }
 }
