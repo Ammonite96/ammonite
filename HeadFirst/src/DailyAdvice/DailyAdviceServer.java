2671e19c2b74477e0e2bac12ef681559810268e2
Ammonite96 <slipknot_rul@mail.ru>
2020-04-14 14:44:28 +0500

    simple chat client

--- HeadFirst/src/DailyAdvice/DailyAdviceServer.java
+++ HeadFirst/src/DailyAdvice/DailyAdviceServer.java
@@ -9,16 +9,16 @@ public class DailyAdviceServer {
     String[] adviceList = {"Еште меньшими порциями", "Купите облигающие джынсы", "Два слова: не годится",
             "Будьте честны хотябы сегодня. Скажите своему начальнику всё, что вы *на самом деле* о нём думаете.",
             "Врзможно, вам стоит подобрать другую причёску"};
-    String advice;
+
     public void go(){
         try {
             ServerSocket serverSocket = new ServerSocket(4242); // Благодаря ServerSocket приложение отслеживает клиентские запросы на порт 4242
             while (true) {
                 Socket sock = serverSocket.accept(); // Метод accept() блокирует приложение до тех пор пока не поступит запрос, после чег овозвращает сокет(на анонимном порту) для взаимодействия с клиентом
                 PrintWriter writer = new PrintWriter(sock.getOutputStream());   // теперь мы используем соединение обьекта Socket с клиентом для создания
-                writer.println(advice); // PrintWriter, после чего отправляем с его помощью строку с ответом. затеим закрываем сокет.
+                writer.println(getAdvice()); // PrintWriter, после чего отправляем с его помощью строку с ответом. затеим закрываем сокет.
                 writer.close();
-                System.out.println(advice);
+                System.out.println(getAdvice());
             }
         }catch(IOException io){
             io.printStackTrace();
---
+++ HeadFirst/src/SimpleChat/SimplechatClientVer1.java
@@ -0,0 +1,56 @@
+package SimpleChat;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SimplechatClientVer1 {
+    JTextField outgoing;
+    PrintWriter writer;
+    Socket sock;
+
+    public void go(){
+        JFrame frame = new JFrame("Simple Chat Client");
+        JPanel mainPanel = new JPanel();
+        outgoing = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        setUpNetworking();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 500);
+        frame.setVisible(true);
+    }
+    private void setUpNetworking(){
+        try{
+            sock = new Socket("127.0.0.1", 5000);
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("networking established");
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+    public class SendButtonListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try{
+                writer.println(outgoing.getText());
+                writer.flush();
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+            outgoing.setText("");
+            outgoing.requestFocus();
+        }
+    }
+
+    public static void main(String[] args) {
+        new SimplechatClientVer1().go();
+    }
+}
