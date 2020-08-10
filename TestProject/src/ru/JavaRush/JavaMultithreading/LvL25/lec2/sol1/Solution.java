05a1e0aabb35d1f96169694d6f7eee026b398984
Ammonite96 <slipknot_rul@mail.ru>
2020-08-10 19:32:47 +0500

    lvl 25 nachalo

---
+++ TestProject/src/ru/JavaRush/JavaMultithreading/LvL25/lec2/sol1/Solution.java
@@ -0,0 +1,33 @@
+package ru.JavaRush.JavaMultithreading.LvL25.lec2.sol1;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Solution {
+    /**
+     * Output:
+     * <p/>
+     * Available Amount
+     * Account Number
+     * Bank Name
+     * --------------------
+     * Available Amount
+     * Bank Name
+     */
+    public static void main(String[] args) {
+
+        Column.configureColumns(Column.Amount, Column.AccountNumber, Column.BankName, Column.Customer);
+        System.out.println(Arrays.toString(Column.values()));
+
+        for (Columnable columnable : Column.getVisibleColumns()) {
+            System.out.println(columnable.getColumnName());
+        }
+
+        System.out.println("--------------------");
+        Column.AccountNumber.hide();
+
+        for (Columnable columnable : Column.getVisibleColumns()) {
+            System.out.println(columnable.getColumnName());
+        }
+    }
+}
