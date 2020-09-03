8d6b869c520f65b8f26e51f10a40ca5a619ff18a
Ammonite96 <slipknot_rul@mail.ru>
2020-09-03 23:06:49 +0500

    Lvl26 JavaChat

--- TestProject/src/ru/JavaRush/JavaMultithreading/LvL26/lec15/JavaChat/Connection.java
+++ TestProject/src/ru/JavaRush/JavaMultithreading/LvL26/lec15/JavaChat/Connection.java
@@ -1,4 +1,44 @@
 package ru.JavaRush.JavaMultithreading.LvL26.lec15.JavaChat;
 
-public class Connection {
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class Connection implements Closeable {
+    private final Socket socket;
+    private final ObjectOutputStream out;
+    private final ObjectInputStream in;
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.in = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void send(Message message) throws IOException {
+        synchronized (out) {
+            out.writeObject(message);
+        }
+    }
+
+    public Message receive() throws IOException, ClassNotFoundException {
+        synchronized (in) {
+            Message message = (Message) in.readObject();
+            return message;
+        }
+    }
+
+    public SocketAddress getRemoteSocketAddress() {
+        SocketAddress address = socket.getRemoteSocketAddress();
+        return address;
+    }
+
+    public void close() throws IOException {
+        socket.close();
+        out.close();
+        in.close();
+    }
 }
--- TestProject/src/ru/JavaRush/JavaMultithreading/LvL26/lec15/JavaChat/Server.java
+++ TestProject/src/ru/JavaRush/JavaMultithreading/LvL26/lec15/JavaChat/Server.java
@@ -1,7 +1,95 @@
 package ru.JavaRush.JavaMultithreading.LvL26.lec15.JavaChat;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+import static ru.JavaRush.JavaMultithreading.LvL26.lec15.JavaChat.MessageType.*;
+
+public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) {
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Сервер запущен");
+            while (true) {
+                Socket socketClient = serverSocket.accept();
+                Handler handler = new Handler(socketClient);
+                handler.start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("Произошла ошибка");
+            e.printStackTrace();
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        connectionMap.forEach((s, connection) -> {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Сообщение не отправленно");
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            Message responseName;
+            while (true) {
+                connection.send(new Message(NAME_REQUEST));
+                responseName = connection.receive();
+                if (responseName.getType() != USER_NAME) {
+                    continue;
+                }
+                if (connectionMap.containsKey(responseName.getData())) {
+                    continue;
+                }
+                if (responseName.getData() == null) {
+                    continue;
+                }
+                if (responseName.getData().isEmpty()) {
+                    continue;
+                }
+                break;
+            }
+            connectionMap.put(responseName.getData(), connection);
+            connection.send(new Message(NAME_ACCEPTED));
+            return responseName.getData();
+        }
+
+        private void notifyUsers(Connection connection, String name) {
+            connectionMap.forEach((s, connection1) -> {
+                try {
+                    if (s != name) {
+                        connection.send(new Message(USER_ADDED, s));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+            while (true) {
+                Message response = connection.receive();
+                if (response.getType() == TEXT) {
+                    String data = String.format("%s: %s", userName, response.getData());
+                    sendBroadcastMessage(new Message(TEXT, data));
+                } else {
+                    ConsoleHelper.writeMessage("Ошибка, сообщение не является текстом");
+                }
+            }
+        }
     }
 }
